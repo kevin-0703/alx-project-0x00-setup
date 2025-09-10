b6d76df7bf3afec5e2461becefbeff0d6162700b
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-10 14:47:57 +0200

    using decorators to extend a functions functionality

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,12 +1,14 @@
 import sqlite3
 import functools 
+from datetime import datetime
 
 def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = kwargs.get('query') or (args[0] if args else None)
         if query:
-            print(f"Executing query: {query}")
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(f"[{timestamp}]Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
