c90d84091c9006614863235edd453b18caf32c59
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-21 13:22:58 +0200

    screating a button component

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+const Button: React.FC<ButtonProps> = ({ title }) => {
+  return (
+    <button className="sm:rounded-sm md:rounded-md rounded-full bg-blue-500 text-white px-3 py-1 text-lg mt-3 sm:">
+      {title}
+    </button>
+  );
+};
+export default Button;
