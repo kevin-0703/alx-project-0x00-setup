1e7b08a244c2931b4658e597e6e6bf95f12205db
KEVIN NSHUTI <nkevin881@gmail.com>
2025-09-21 13:27:10 +0200

    screating a button component

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,7 +5,7 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button />
+      <Button className="rounded-sm rounded-lg rounded-full" />
     </div>
   );
 };
